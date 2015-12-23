8f401d9c99f21bf758e29a47b9a46317892ca465
Ted Pyne <ted.pyne.4@gmail.com>
2015-12-22 21:27:01 -0500

    Multivar console version

--- src/integrator/MultiVar.java
+++ src/integrator/MultiVar.java
@@ -2,12 +2,13 @@ package integrator;
 
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
 import evaluator.Evaluator;
 import evaluator.MutableDouble;
 
 public class MultiVar {
-	public static void main(String[] args){
+	public static void main(String[] args) throws Exception{
 		Scanner scan = new Scanner(System.in);
 		System.out.print("INTEGRAND: ");
 		String equation = scan.next();
@@ -16,12 +17,41 @@ public class MultiVar {
 		eval.parse(equation);
 		
 		Map<String, MutableDouble> vars = eval.getKeys();
+		Set<String> varNames = vars.keySet();
 		int numVars = vars.size();
 		if(numVars==0) return;
 		
-		int[][] varBounds = new int[numVars][2];		//Lower and upper bound for each var
-		for(int i = 0; i < numVars; i++){
-			System.out.println("Lower bound for " + )
+		double[][] varBounds = new double[numVars][2];		//Lower and upper bound for each var
+		int i = 0;
+		for(String varName: varNames){
+			System.out.println("Lower bound for " + varName);
+			varBounds[i][0] = scan.nextDouble();
+			System.out.println("Upper bound for " + varName);
+			varBounds[i][1] = scan.nextDouble();
+			i++;
 		}
+		
+		//Now do the parsing
+		System.out.print("NUM SAMPLES: ");
+		int samples = scan.nextInt();
+		double sum = 0;
+		for(int j = 0; j < samples; j++){
+			int k = 0;
+			for(String var: varNames){
+				vars.get(var).setVal(randInBound(varBounds[k]));
+				k++;
+			}
+			sum+=eval.evaluate(vars);
+		}
+		sum/=samples;
+		for(double[] bound: varBounds){
+			sum*=bound[1]-bound[0];
+		}
+		
+		System.out.println("SUM FOUND TO BE: " + sum);
+	}
+	
+	public static double randInBound(double[] arr){
+		return arr[0] + (Math.random()*(arr[1]-arr[0]));
 	}
 }
